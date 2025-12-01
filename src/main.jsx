4ff50049667ebb9d0076d240c834c000292f51ba
marcevillagra58-gif <marcevillagra58@gmail.com>
2025-12-01 20:54:41 -0300

    Add basename to BrowserRouter for GitHub Pages routing

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,8 @@ import { TransitionProvider } from './components/Transition.jsx'
 import { AuthProvider } from './context/AuthContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>  
-    <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+  <StrictMode>
+    <BrowserRouter basename="/HURLINGHAM_PNO_REACT" future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
       <AuthProvider>
         <TransitionProvider>
           <App />
